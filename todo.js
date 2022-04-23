87fd5e1df26e198211ccd909bf5f0ba48a3feb50
notTali <mtalifhani54@gmail.com>
2022-04-23 19:03:43 +0200

    js task list (demo version)

--- todo.js
+++ todo.js
@@ -1,1 +1,12 @@
-// const 
+const list = [
+    "Task 1",
+    "Task 2",
+    "Task 3",
+    "Task 4",
+    "Task 5",
+    "Task 6"
+];
+
+const ul = document.querySelector(".all-tasks");
+// console.log(ul);
+
